2bb2d6464b5b66d160eedc45cd6c66d7a8c984d6
C-M-Vaibhav <cmvaibhav8904@gmail.com>
2026-07-04 09:30:06 +0530

    initial commit

---
+++ swamicloudkitchen/src/main/java/com/cloudkitchen/swamicloudkitchen/SwamicloudkitchenApplication.java
@@ -0,0 +1,13 @@
+package com.cloudkitchen.swamicloudkitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwamicloudkitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwamicloudkitchenApplication.class, args);
+	}
+
+}
